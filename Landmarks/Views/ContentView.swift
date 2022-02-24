34068755b0cb0c5ab73b50d8954bf3ecf0c195b9
“jdocampom” <gummy_pond0v@icloud.com>
2022-02-24 12:18:00 -0500

    Checkpoint 3: Handling User Input

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Juan Diego Ocampo on 24/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
